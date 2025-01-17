88b8f9a1a3a3ff5cc29d75b5627b4a522e8bddbd
TimurTimergalin <timur20041229@gmail.com>
2025-01-17 15:06:54 +0300

    исправлен mergeMoves

--- tests/moves.test.ts
+++ tests/moves.test.ts
@@ -14,3 +14,15 @@ test("mergeMoves:repeated destination", () => {
         expect(moves.includes(move)).toBeTruthy()
     }
 })
+
+test("mergeMoves:all merged", () => {
+    const moves: Move<number>[] = [
+        {from: 1, to: 4},
+        {from: 4, to: 7},
+        {from: 7, to: 10},
+        {from: 10, to: 13}
+    ]
+
+    const merged = mergeMoves(moves)
+    expect(merged).toEqual([{from: 1, to: 13}])
+})
