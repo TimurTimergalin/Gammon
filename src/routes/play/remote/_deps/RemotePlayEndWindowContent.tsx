3f3b3a5c1f8bce5bfb85351dee64ad6ce7322bc9
TimurTimergalin <timur20041229@gmail.com>
2025-05-18 21:25:11 +0300

    Кнопка перехода на анализ в конце игры

--- src/routes/play/remote/_deps/RemotePlayEndWindowContent.tsx
+++ src/routes/play/remote/_deps/RemotePlayEndWindowContent.tsx
@@ -1,7 +1,8 @@
 import {ComponentProps} from "react";
-import {useNavigate} from "react-router";
-import {endWindowContentStyle, returnToMenuButtonStyle} from "../../_deps/styles";
+import {useNavigate, useParams} from "react-router";
+import {endWindowButtonStyle, endWindowContentStyle, returnToMenuButtonStyle} from "../../_deps/styles";
 import styled from "styled-components";
+import {AccentedButton} from "../../../../components/AccentedButton";
 
 
 const ReturnToMenuButton = (props: ComponentProps<"button">) => {
@@ -16,11 +17,24 @@ const StyledReturnToMenuButton = styled(ReturnToMenuButton)`
     ${returnToMenuButtonStyle}
 `
 
+const PlainAnalysisButton = ({className}: {className?: string}) => {
+    const navigate = useNavigate()
+    const {roomId} = useParams()
+
+    return (
+        <AccentedButton className={className} onClick={() => navigate(`/history/${roomId}`)}>Анализ</AccentedButton>
+    )
+}
+
+const AnalysisButton = styled(PlainAnalysisButton)`
+    ${endWindowButtonStyle}
+`
 
 const PlainRemotePlayEndWindowContent = ({className}: {className?: string}) => {
     return (
         <div style={{display: "flex", flexDirection: "column"}}>
             <div className={className}>
+                <AnalysisButton />
                 <StyledReturnToMenuButton />
             </div>
         </div>
